70d9908dcae8ce3b02679813da229392ca407394
Archana Mondal <73637034+archanamondal@users.noreply.github.com>
2021-12-22 01:00:13 +0530

    Blog Page Added (#1)
    * blog page added
    
    * blog page updated

--- components/blog/blog.component.tsx
+++ components/blog/blog.component.tsx
@@ -19,9 +19,9 @@ const Blog: React.FC<BlogComponentProps> = (props: BlogComponentProps) => {
           <p className="text-sm text-gray-600">November 2, 2021</p>
         </div>
         <p className="py-2 text-xl font-medium hover:text-orange-500 hover:cursor-pointer">Keep your brand consistent with an Illustration Style guide</p>
-        <p className="pb-5 text-base text-gray-700">The scenario: The team is growing, with multiple personas creating your brand's visual content. Your Brand Book is a great source of visual</p>
+        <p className="pb-5 text-base text-gray-700">The scenario: The team is growing, with multiple personas creating your brand&apos;s visual content. Your Brand Book is a great source of visual</p>
         <div className="flex flex-row pb-5 items-center">
-            <Image className="rounded-full" src="/avatar.jpg" height={50} width={50} />
+            <Image className="rounded-full" src="/avatar.jpg" alt="" height={50} width={50} />
             <div className="px-3 flex flex-col">
                 <p className="text-sm font-medium text-gray-700">Chanchal Sadhukhan</p>
                 <p className="text-sm text-gray-600">App Developer</p>
