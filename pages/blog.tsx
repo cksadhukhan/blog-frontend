6afa2340cbc13285385c36b58d3a7c510bc486cf
archanamondal <archanamondal995@gmail.com>
2021-12-22 00:55:29 +0530

    blog page updated

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Navbar from "../components/navbar/navbar.component";
+import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
-import { Author } from "../models";
+import Navbar from "../components/navbar/navbar.component";
 import Blog from "../components/blog/blog.component";
+import { Author } from "../models";
 
-const BlogPage = () => {
+const BlogPage: NextPage = () => {
   return (
     <>
       <Navbar />
-      <div className="px-3 lg:px-[20rem] pt-2 lg:pt-10">
+      <div className="px-4 lg:px-[20rem] pt-2 lg:pt-10">
         <div className="py-1 lg:px-10 flex flex-row items-center">
           <p className="text-md text-gray-600">Development</p>
           <p className="px-2 text-lg text-gray-600">•</p>
@@ -47,20 +47,22 @@ const BlogPage = () => {
           <Image
             height={300}
             width={700}
-            layout="fixed"
+            layout="responsive"
             src="/blogimg1.png"
             alt=""
           />
         </div>
-        <div className="lg:px-10">
-          <h2 className="lg:py-10 text-3xl font-medium">A team effort in a Box</h2>
+        <div className="px-3 lg:px-10">
+          <h2 className="py-4 lg:py-10 text-3xl font-medium">
+            A team effort in a Box
+          </h2>
           <p className="lg:pb-10 text-xl text-gray-800 font-['alegreya'] leading-8 tracking-wide">
             This Pixel Kit represents months of collaboration. From the
             Marketing to the People Operations team, everyone did their best to
             embody the Pixel culture in a small package, and we can&apos;t wait
             to see it being used out there.
           </p>
-          <p className="lg:py-16 text-xl font-bold italic text-gray-700">
+          <p className="py-4 lg:py-16 text-xl font-bold italic text-gray-700">
             Follow Pixelmatters on{" "}
             <Link href="/" passHref>
               <a className="text-orange-600 underline hover:opacity-50">
@@ -87,7 +89,7 @@ const BlogPage = () => {
             </Link>
           </p>
           <hr className="text-gray-300" />
-          <div className="flex flex-row justify-between pb-16">
+          <div className="flex flex-col justify-center items-center md:flex-row lg:flex-row md:justify-between lg:justify-between pb-16">
             <div className="flex flex-row pb-5 items-center py-7">
               <Image
                 className="rounded-full"
@@ -123,10 +125,12 @@ const BlogPage = () => {
         </div>
       </div>
       <div className="bg-slate-100">
-          <div className="lg:px-40 lg:py-14">
-            <h2 className="text-4xl font-medium text-gray-800">Related blog posts</h2>
-          </div>
-          <div className="px-4 lg:px-40 py-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <div className="px-4 py-4 lg:px-40 lg:py-14">
+          <h2 className="text-4xl font-medium text-gray-800">
+            Related blog posts
+          </h2>
+        </div>
+        <div className="px-4 lg:px-40 py-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
           <Blog
             title="Keep your brand consistent with an Illustration Style guide"
             description="The scenario: The team is growing, with multiple personas creating your brand's visual content. Your Brand Book is a great source of visual"
@@ -148,7 +152,7 @@ const BlogPage = () => {
             tag="Development"
             author={new Author("Chanchal Sadhukhan", "App Developer")}
           />
-          </div>
+        </div>
       </div>
     </>
   );
