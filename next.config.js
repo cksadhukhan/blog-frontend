41153349bf7bea80a004de5c5466772c33309109
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2022-01-08 18:07:18 +0530

    redirect added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io", "www.google.com"],
+    domains: ["cdn.sanity.io"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/page",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/page/1",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/profile",
+        destination: "/",
+        permanent: true,
+      },
+    ];
   },
 };
