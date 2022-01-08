c987e0f25b8333e907c0c4310760f08932eed2d2
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2022-01-08 18:07:58 +0530

    error page updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,25 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Layout } from "../components";
 
 const NotFound: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Blog App</title>
         <meta name="description" content="Blog app description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="flex flex-col py-10 items-center justify-center ">
-          <div className=" flex flex-col lg:flex-row">
+        <div className="flex flex-col py-10 px-28">
+          <div className="flex flex-col lg:flex-row justify-evenly">
             <Image
-              className="rounded mx-5"
-              height={400}
-              width={600}
-              src="/404.png"
+              className="rounded"
+              height={350}
+              width={450}
+              src="/error.png"
               alt="banner"
             />
             <div className="px-3 my-6">
@@ -41,7 +42,9 @@ const NotFound: NextPage = () => {
 
                 <div className="flex flex-row items-center justify-center">
                   <button className="text-md py-2 px-3 mr-3 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded-full ">
-                    Back to Homepage
+                    <Link href="/">
+                      <a>Back to Homepage</a>
+                    </Link>
                   </button>
 
                   <button className="text-md py-2 px-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded-full">
@@ -53,7 +56,7 @@ const NotFound: NextPage = () => {
           </div>
         </div>
       </Layout>
-    </div>
+    </>
   );
 };
 
