e221db1d1595329ba1000be47df1c55756269524
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2022-01-08 18:07:31 +0530

    pagination component updated

--- components/pagination/pagination.component.tsx
+++ components/pagination/pagination.component.tsx
@@ -1,21 +1,24 @@
 import { NextPage } from "next";
+import Link from "next/link";
+import React from "react";
 import { PaginationComponentProps } from "./pagination.component.props";
 
 export const Pagination: NextPage<PaginationComponentProps> = (
   props: PaginationComponentProps
 ) => {
   const { currentPage, totalPage } = props;
+
   const isFirstPage = (): boolean => {
-    return currentPage === 1;
+    return Number(currentPage) === 1;
   };
   const isLastPage = (): boolean => {
-    return currentPage === totalPage;
+    return Number(currentPage) === Number(totalPage);
   };
   return (
     <div className="flex flex-row px-40 py-16 items-center justify-center">
       {!isFirstPage() && (
         <button className="text-md bg-transparent bg-white font-medium text-gray-800 py-2 px-3 border hover:border-gray-800 border-transparent hover:border rounded-full">
-          Previous posts
+          <Link href={`/page/${Number(currentPage) - 1}`}>Previous posts</Link>
         </button>
       )}
       <p className="text-gray-800 px-5">
@@ -23,7 +26,7 @@ export const Pagination: NextPage<PaginationComponentProps> = (
       </p>
       {!isLastPage() && (
         <button className="text-md bg-transparent bg-white font-medium text-gray-800 py-2 px-3 border hover:border-gray-800 border-transparent hover:border rounded-full">
-          Next posts
+          <Link href={`/page/${Number(currentPage) + 1}`}>Next posts</Link>
         </button>
       )}
     </div>
