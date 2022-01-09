1ea61eac1ba3195f4095cc12d10c0683e5dae752
archanamondal <archanamondal995@gmail.com>
2022-01-10 02:12:34 +0530

    about image added

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "../components";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>About Us</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default AboutPage;
